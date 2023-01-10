5d84173db281fd68216d281d30370c346ff55607
Damian <damian.wojcik@multiway.pl>
2023-01-10 09:21:04 +0100

    Changes: - Added min/max constrains in requiredFoodPerDay field in AnimalType model - Moved public methods up in ZoneService

--- src/main/java/com/zoo/model/AnimalType.java
+++ src/main/java/com/zoo/model/AnimalType.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.util.Set;
 
@@ -20,6 +22,8 @@ public class AnimalType {
     private String name;
 
     @Column(name = "required_food_per_day")
+    @Min(1)
+    @Max(99)
     private int requiredFoodPerDay;
 
     @OneToMany(mappedBy = "animalType")
